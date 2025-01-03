c5c42d1468abe081674ab70a6b022c85c69210d8
Leo Bottaro <github@leobottaro.com>
2025-01-03 17:30:44 -0300

    Add Rider-Avalonia base project

---
+++ Code/Windows/MA3.FaderWing/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace FW.Bridge.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Code/Windows/MA3.FaderWing/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace FW.Bridge.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
