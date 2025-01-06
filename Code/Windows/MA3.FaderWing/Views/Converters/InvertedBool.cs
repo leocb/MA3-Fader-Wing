16f710447eb4b0f9c0e10d1c6dd39adc99ec7ca8
Leo Bottaro <github@leobottaro.com>
2025-01-06 01:59:42 -0300

    Device Button View User Control

---
+++ Code/Windows/MA3.FaderWing/Views/Converters/InvertedBool.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace FW.Bridge.Views.Converters;
+
+public class InvertedBoolConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return !(bool) value;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return !(bool) value;
+    }
+}
---
+++ Code/Windows/MA3.FaderWing/Views/UserControls/DeviceButtonView.axaml.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace FW.Bridge.Views.UserControls;
+
+public partial class DeviceButtonView : UserControl
+{
+    public DeviceButtonView()
+    {
+        InitializeComponent();
+    }
+    
+    public static readonly StyledProperty<string?> ButtonCodeProperty =
+        AvaloniaProperty.Register<DeviceButtonView,string?>(nameof(ButtonId));
+
+    public string? ButtonId
+    {
+        get => GetValue(ButtonCodeProperty);
+        set => SetValue(ButtonCodeProperty, value);
+    }
+    
+    
+    public static readonly StyledProperty<bool> IsPressedProperty =
+        AvaloniaProperty.Register<DeviceButtonView,bool>(nameof(IsPressed));
+    
+    public bool IsPressed
+    {
+        get => GetValue(IsPressedProperty);
+        set => SetValue(IsPressedProperty, value);
+    }
+}
